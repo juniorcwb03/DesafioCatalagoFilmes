bd59d37b59bc61c4870cb32262ee02a926d8b1e2
Junior <Junior@DESKTOP-IUJ2GC7>
2021-07-18 21:28:44 -0300

    Adicionar arquivos de projeto.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DesafioCatalagoFilmes.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+        public DbSet<Filme> Filmes{ get; set; }
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DesafioCatalagoFilmes.Models
+{
+    public class Filme
+    {
+        public int FilmeId { get; set; }
+        public string Titulo { get; set; }
+        public string DataLancamento { get; set; }
+        public string GeneroFilme { get; set; }
+        public string Diretor { get; set; }
+        public string ListaAutores { get; set; }
+    }
+}
